ad7a6467828fbe28e68f5e84a1842fdba7100257
daniell0154 <danieldejesusferreira01@gmail.com>
2025-08-15 10:14:18 -0300

    Mudando arquivos .java para .kt

---
+++ app/src/main/java/com/example/projetosaveit/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.projetosaveit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.projetosaveit.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home,
+            R.id.navigation_vitrine,
+            R.id.navigation_notifications,
+            R.id.relatorio,
+            R.id.perfil
+        )
+            .build()
+        val navController = findNavController(this, R.id.nav_host_fragment_activity_main)
+        setupActionBarWithNavController(this, navController, appBarConfiguration)
+        setupWithNavController(binding!!.navView, navController)
+    }
+}
---
+++ app/src/main/java/com/example/projetosaveit/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,38 @@
+package com.example.projetosaveit.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.projetosaveit.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+    private var binding: FragmentNotificationsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding!!.root
+
+        val textView = binding!!.textNotifications
+        notificationsViewModel.text.observe(
+            viewLifecycleOwner
+        ) { text: String? ->
+            textView.text =
+                text
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
