89be3e5e8d2542589340d8ec159cfd8719b8b537
daniell0154 <danieldejesusferreira01@gmail.com>
2025-08-15 10:36:19 -0300

    Refatorando a SplashScreen

--- app/src/main/java/com/example/projetosaveit/SplashScreen.kt
+++ app/src/main/java/com/example/projetosaveit/SplashScreen.kt
@@ -7,11 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SpashScreen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.enableEdgeToEdge()
-        setContentView(R.layout.activity_spash_screen)
+        setContentView(R.layout.activity_splash_screen)
         ViewCompat.setOnApplyWindowInsetsListener(
             findViewById(R.id.main)
         ) { v: View, insets: WindowInsetsCompat ->
