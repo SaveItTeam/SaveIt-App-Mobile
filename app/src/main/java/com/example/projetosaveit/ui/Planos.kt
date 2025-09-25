0cedce5c6b5ffce886317403e3dcc0eea5e226e8
daniell0154 <danieldejesusferreira01@gmail.com>
2025-09-25 12:15:10 -0300

    Adicionando tela de planos e de inserir funcionario

--- app/src/main/java/com/example/projetosaveit/ui/Planos.kt
+++ app/src/main/java/com/example/projetosaveit/ui/Planos.kt
@@ -1,11 +1,15 @@
 package com.example.projetosaveit.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projetosaveit.R
+import com.example.projetosaveit.ui.perfil.Perfil
 
 class Planos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,18 @@ class Planos : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.btBasico).setOnClickListener {
+            Toast.makeText(this, "Plano Básico assinado!", Toast.LENGTH_SHORT).show()
+            val Intent = Intent(this, Perfil::class.java)
+            startActivity(Intent)
+        }
+
+        findViewById<Button>(R.id.btAvancado).setOnClickListener {
+            Toast.makeText(this, "Plano Avançado assinado!", Toast.LENGTH_SHORT).show()
+            val Intent = Intent(this, Perfil::class.java)
+            startActivity(Intent)
         }
     }
 }
